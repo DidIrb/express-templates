70c9dd0ad0a45a6d2d196d557b36f3dbf983edc7
DidIrb <irbayedubadida@gmail.com>
2024-08-22 00:44:02 +0300

    feat: allowed cli to copy js base template

---
+++ CONTRIBUTIONS.md
@@ -0,0 +1,14 @@
+## NOTE
+
+In order to allow for any package manager to be used, we added
+.npmrc in each file and added `save=false` flag
+
+To prevent the monorepo from updating constantly
+
+Incase you need to install a package please use `-save` flag
+
+```bash
+npm install express --save
+```
+
+
--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
-
 import inquirer from 'inquirer';
+import fs from 'fs-extra';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const questions = [
   {
@@ -12,5 +17,22 @@ const questions = [
 ];
 
 inquirer.prompt(questions).then((answers) => {
-  console.log(`You chose: ${answers.language}`);
+  const templateDir = path.resolve(__dirname, '../../../templates', answers.language.toLowerCase());
+  const targetDir = process.cwd();
+
+  if (answers.language === 'JavaScript' || answers.language === 'TypeScript') {
+    copyTemplate(templateDir, targetDir);
+  } else {
+    console.log(`Template for ${answers.language} is not yet implemented.`);
+  }
 });
+
+function copyTemplate(src: string, dest: string) {
+  fs.copy(src, dest, {
+    filter: (src, dest) => {
+      return !src.includes('node_modules');
+    }
+  })
+  .then(() => console.log('Template copied successfully!'))
+  .catch(err => console.error('Error copying template:', err));
+}
