24b4c366d1fa00bf79b0d8cdf971e25efdbecf29
didirb <irbayedubadida@gmail.com>
2024-09-25 16:44:19 +0300

    refactor(exirdjs): set up eslint, prettier and husky to lint code before commits

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import prettierPlugin from "eslint-plugin-prettier"
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: { "prettier/prettier": "error" },
+  },
+  {
+    ignores: [
+      "node_modules",
+      "eslint.config.mjs",
+      "apps",
+      "**/dist",
+      "config/*",
+    ],
+  },
+]
